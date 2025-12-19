2697f3efd711f9dd6eb260a0553efba3138cfce9
village-coders <villagecoders7@gmail.com>
2025-12-20 00:06:29 +0100

    kjhjk

--- Model/message.js
+++ Model/message.js
@@ -5,7 +5,7 @@ const messageSchema = new mongoose.Schema({
     email: {type: String, required: true},
     message: {type: String, required: true},
     phone: {type: String, required: true},
-    services: {type: String, enum: ["website development", "cctv installation", "technical support", "consultation"], required: true}
+    service: {type: String, enum: ["website development", "cctv installation", "technical support", "consultation"], required: true}
 })
 const messageModel = mongoose.model('message', messageSchema)
 module.exports = messageModel
